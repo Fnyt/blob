e2a75d1bafe4f2cd59ad624673a10489be81504e
Fnyt <fnytyoyo@gmail.com>
2016-11-24 01:35:59 +0100

    Player moving and jumping (non-grounded).

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 position = transform.position;
+        position.x = Player.transform.position.x;
+        transform.position = position;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody2D rb;
+
+    private int _jumpforce = 200;
+    private float _movespeed = 2.5f;
+ 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * _movespeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * _movespeed * Time.deltaTime);
+        }
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            rb.AddForce(Vector2.up * _jumpforce);
+        }
+	}
+}
