0102ee331c55853216f1f6e59de354b11f626278
Fnyt <fnytyoyo@gmail.com>
2016-11-25 22:41:33 +0100

    Ground stuck fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour {
     private float _movespeed = 2.5f;
 	private bool _facingright = true;
 	private bool _grounded = true;
+
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
@@ -20,22 +22,36 @@ public class PlayerController : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-			transform.Translate(Vector3.left * _movespeed * Time.deltaTime);
+			//transform.Translate(Vector3.left * _movespeed * Time.deltaTime);
 			GetComponent<SpriteRenderer> ().flipX = true;
-			//rb.velocity += Vector2.left * _movespeed;
+			rb.velocity += Vector2.left * _movespeed;
 			_facingright = false;
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(Vector3.right * _movespeed * Time.deltaTime);
+            //transform.Translate(Vector3.right * _movespeed * Time.deltaTime);
 			GetComponent<SpriteRenderer> ().flipX = false;
-			//rb.velocity += Vector2.right * _movespeed;
+			rb.velocity += Vector2.right * _movespeed;
 			_facingright = true;
         }
 		if(Input.GetKeyDown(KeyCode.Z) && _grounded)
         {
             rb.AddForce(Vector2.up * _jumpforce);
         }
+	}
+
+	void OnCollisionStay2D(Collision2D col)
+	{
+		Vector3 dir = (col.gameObject.transform.position - gameObject.transform.position).normalized;
+		Debug.Log (dir.ToString ());
+		if (dir == Vector3.left)
+		{
+			Debug.Log ("Hit left side");
+		}
+		else if (dir == Vector3.right)
+		{
+			Debug.Log ("Hit right side");
+		}
 	}
 }
