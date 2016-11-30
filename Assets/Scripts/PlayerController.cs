6a72a870f3739bf33bef96762e43998596ccbe3a
Fnyt <fnytyoyo@gmail.com>
2016-11-30 22:33:31 +0100

    Blob animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,28 +3,29 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	private Rigidbody2D rb;
+	private Rigidbody2D _rb;
     private int _jumpforce = 200;
     private float _movespeed = 2.5f;
 	private bool _facingright = true;
 	private bool _grounded = true;
-
+    private Animator _animator;
 
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody2D>();
+		_rb = GetComponent<Rigidbody2D>();
+		_animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		rb.velocity = new Vector2 (0f, rb.velocity.y);
-		_grounded = (rb.velocity.y == 0);
+		_rb.velocity = new Vector2 (0f, _rb.velocity.y);
+		_grounded = (_rb.velocity.y == 0);
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
 			//transform.Translate(Vector3.left * _movespeed * Time.deltaTime);
 			GetComponent<SpriteRenderer> ().flipX = true;
-			rb.velocity += Vector2.left * _movespeed;
+			_rb.velocity += Vector2.left * _movespeed;
 			_facingright = false;
         }
 
@@ -32,26 +33,23 @@ public class PlayerController : MonoBehaviour {
         {
             //transform.Translate(Vector3.right * _movespeed * Time.deltaTime);
 			GetComponent<SpriteRenderer> ().flipX = false;
-			rb.velocity += Vector2.right * _movespeed;
+			_rb.velocity += Vector2.right * _movespeed;
 			_facingright = true;
         }
 		if(Input.GetKeyDown(KeyCode.Z) && _grounded)
         {
-            rb.AddForce(Vector2.up * _jumpforce);
+            _rb.AddForce(Vector2.up * _jumpforce);
+            _grounded = false;
+            _animator.SetInteger("State", 2);
         }
-	}
+        if (_grounded)
+            _animator.SetInteger("State", _rb.velocity.x == 0 ? 0 : 1);
+
+
+    }
 
 	void OnCollisionStay2D(Collision2D col)
 	{
-		Vector3 dir = (col.gameObject.transform.position - gameObject.transform.position).normalized;
-		Debug.Log (dir.ToString ());
-		if (dir == Vector3.left)
-		{
-			Debug.Log ("Hit left side");
-		}
-		else if (dir == Vector3.right)
-		{
-			Debug.Log ("Hit right side");
-		}
+
 	}
 }
