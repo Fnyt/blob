6abd515bf4c70f63bf59e4e5dba45491e8267719
studentVR <studentVR>
2016-12-01 19:11:54 +0100

    Basic GUI stuff

---
+++ Assets/Scripts/GUIController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GUIController : MonoBehaviour {
+
+	public void Exit()
+	{
+		Application.Quit ();
+	}
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene ("MainScene");
+	}
+}
