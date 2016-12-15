663bf0fe89353c07eaad647e904ef0f053d521dd
Fnyt <fnytyoyo@gmail.com>
2016-12-15 12:19:26 +0100

    Timed Door assets

---
+++ Assets/Scripts/TimedButtonController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedButtonController : MonoBehaviour {
+
+    private bool _pressed = false;
+    private bool _playerin = false;
+    public Sprite PressedSprite;
+    public GameObject ConnectedDoor;
+
+    public void PlayerEntered()
+    {
+        _playerin = true;
+    }
+
+    public void PlayerLeft()
+    {
+        _playerin = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X) && !_pressed && _playerin)
+        {
+            Press();
+        }
+    }
+
+    public void Press()
+    {
+        _pressed = true;
+        GetComponent<SpriteRenderer>().sprite = PressedSprite;
+        StartCoroutine(ConnectedDoor.GetComponent<DoorController>().OpenDoor());
+    }
+}
---
+++ Assets/Scripts/TimedDoorController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedDoorController : MonoBehaviour {
+
+    public float OpenedTime;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public IEnumerator OpenDoor()
+    {
+        _animator.SetBool("IsOpen", true);
+        yield return new WaitForSeconds(OpenedTime);
+        _animator.SetBool("IsOpen", false);
+    }
+}
